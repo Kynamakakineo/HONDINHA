74dc5f3317e1004876d8bc4beb3764f3562c2d01
Kynamakakineo <cainan.rsepriano@senacsp.edu.br>
2025-11-25 21:49:21 -0300

    Primeiro commit da API

---
+++ Models/Concessionaria.cs
@@ -0,0 +1,15 @@
+namespace AutoManagerApi.Models
+{
+
+    //Olha a tabela d concessionarias
+    public class Concessionaria
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; } = null!;
+        public string Endereco { get; set; } = null!;
+        public string Telefone { get; set; } = null!;
+        public string Cnpj { get; set; } = null!;
+
+        public List<Carro> Carros { get; set; } = new();
+    }
+}
